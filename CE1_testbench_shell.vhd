736cf391e90ce5eae44e712a323768545ee11827
KevinCabusora <c16kevin.cabusora@usafa.edu>
2014-01-29 11:29:21 -0700

    Update Testbench 1129 Jan29

--- CE1_testbench_shell.vhd
+++ CE1_testbench_shell.vhd
@@ -83,7 +83,43 @@ BEGIN
 		A <= '0';
 		B <= '0';
 		C <= '1';
-		wait for 100 ns;	
+		wait for 100 ns;
+		
+		-- Third line of truth table
+		A <= '0';
+		B <= '1';
+		C <= '0';
+		wait for 100 ns;
+		
+		-- Fourth line of truth table
+		A <= '0';
+		B <= '1;
+		C <= '1';
+		wait for 100 ns;
+		
+		-- Fifth line of truth table
+		A <= '1';
+		B <= '0';
+		C <= '0';
+		wait for 100 ns;
+		
+		-- Sixth line of truth table
+		A <= '1';
+		B <= '0';
+		C <= '1';
+		wait for 100 ns;
+		
+		-- Seventh line of truth table
+		A <= '1';
+		B <= '1';
+		C <= '0';
+		wait for 100 ns;
+		
+		-- Eighth line of truth table
+		A <= '1';
+		B <= '1';
+		C <= '1';
+		wait for 100 ns;
 		
 		-- Complete_ the rest of the truth table below:
 
